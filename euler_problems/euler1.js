29d2215b0d8fcc45ab3c1376b64616dcfc952d7e
Eric Champion <ericmchampion@gmail.com>
2015-06-16 22:20:32 -0400

    Euler1
    Added solution for first euler problem

---
+++ euler_problems/euler1.js
@@ -0,0 +1,11 @@
+function euler(total){
+  var total = 0;
+  for(var i = 1; i < 1000; i++){
+    if(i % 3 === 0  || i % 5 === 0){
+      total += i;
+    }
+  }
+  return total;
+}
+
+console.log(euler());
